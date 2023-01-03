cb3a271606b9e6cbea1d2c9bb50894cfcc9bbfcb
Manuel Lehé <manuellehe@googlemail.com>
2023-01-03 21:39:57 +0100

    Initial commit

---
+++ Zettel/Zettel/ZettelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZettelApp.swift
+//  Zettel
+//
+//  Created by Manuel Lehé on 03.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ZettelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
